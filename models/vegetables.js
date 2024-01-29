aff1514b77cabb29da7f9387102a3808f876ea4d
bristeele99 <bristeele99@Gmail.com>
2024-01-29 13:51:29 -0500

    first commit

---
+++ models/vegetables.js
@@ -0,0 +1,19 @@
+const vegetables = [
+    {
+        name:'brocolli',
+        color: 'green',
+        readyToEat: true
+    },
+    {
+        name:'leek',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        name:'onion',
+        color: 'red',
+        readyToEat: true
+    }
+]; 
+
+module.exports = vegetables;
